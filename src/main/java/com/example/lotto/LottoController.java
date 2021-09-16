4b9a04b2681f2aa48cc0f62116ffae501a0eddaf
David Denny <david.denny@lexisnexisrisk.com>
2021-09-16 11:51:05 +0100

    Rename from auto generated

--- src/main/java/com/example/lotto/LottoController.java
+++ src/main/java/com/example/lotto/LottoController.java
@@ -3,7 +3,7 @@ package com.example.lotto;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LottoController {
     @FXML
     private Label welcomeText;
 
