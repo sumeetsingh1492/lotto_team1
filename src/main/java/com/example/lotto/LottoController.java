b3469993c14839d5bb0c3f464c9f141b4050be02
David Denny <david.denny@lexisnexisrisk.com>
2021-09-16 13:16:19 +0100

    Add basic grid

--- src/main/java/com/example/lotto/LottoController.java
+++ src/main/java/com/example/lotto/LottoController.java
@@ -1,5 +1,6 @@
 package com.example.lotto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,11 @@ public class LottoController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onResetButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onGenerateButtonClick(ActionEvent actionEvent) {
     }
 }
